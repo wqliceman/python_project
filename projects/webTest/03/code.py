0773ec2d7b8943be883d712ecdd253954208ee52
wqliceman <wqliceman@gmail.com>
2015-06-29 19:08:39 +0800

    修改代码，可进行搜索

--- projects/webTest/03/code.py
+++ projects/webTest/03/code.py
@@ -3,16 +3,32 @@ __author__ = 'wqliceman'
 import web
 
 urls =(
-    '/', 'index'
+    '/', 'index',
+    '/movie/(\d+)', 'movie'
 )
+
+render = web.template.render('templates/')
+db = web.database(dbn='sqlite', db='MovieSite.db')
 
 class index():
     def GET(self):
-        render = web.template.render('templates/')
-        db = web.database(dbn='sqlite', db='MovieSite.db')
         movies = db.select('movie')
         print movies
         return render.index(movies)
+    def POST(self):
+        data = web.input()
+        condition = r'title like "%'+data.title+'%"'
+        movies = db.select('movie', where=condition)
+        return render.index(movies)
+
+
+class movie():
+    def GET(self, movie_id):
+        movie_id = int(movie_id)
+        movie = db.select('movie', where='id=$movie_id', vars=locals())[0]
+        print movie
+        return render.movie(movie)
+
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
