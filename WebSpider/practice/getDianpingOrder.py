5ccec11e7efb54674136f837cc5f4a4875d047f4
wqliceman <wqliceman@gmail.com>
2015-07-23 09:17:58 +0800

    get dianping.com order

---
+++ WebSpider/practice/getDianpingOrder.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+__author__ = 'wqliceman'
+
+import urllib
+import urllib2
+import re
+import cookielib
+import sys
+import  os
+
+#1. get login capcha url
+
+#2. get login capcha images
+
+#3. login
+
+#4. goto account pages
+
+
